b6d58c81cfeba326177246bd61095f21c60b7983
v801 <32247825+v801@users.noreply.github.com>
2022-07-23 16:55:36 -0600

    found this on my hard drive, probably doesnt work but i never posted these source files online before

---
+++ LightningArrows/LightningArrowPrefab.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using BepIn;
+using JotunnLib.Entities;
+using UnityEngine;
+
+namespace LightningArrows
+{
+    public class LightningArrowPrefab : PrefabConfig
+    {
+        public LightningArrowPrefab() : base("ArrowLightning", "ArrowSilver")
+        {
+
+        }
+
+        public override void Register()
+        {
+            ItemDrop item = Prefab.GetComponent<ItemDrop>();
+            item.m_itemData.m_shared.m_itemType = ItemDrop.ItemData.ItemType.Ammo;
+            item.m_itemData.m_shared.m_name = "Lightning Arrow";
+            item.m_itemData.m_shared.m_description = "Shockingly fast.";
+            item.m_itemData.m_dropPrefab = Prefab;
+            item.m_itemData.m_shared.m_weight = 0.1f;
+            item.m_itemData.m_shared.m_maxStackSize = 100;
+            item.m_itemData.m_shared.m_variants = 1;
+            item.m_itemData.m_shared.m_damages.m_pierce = 20;
+            item.m_itemData.m_shared.m_damages.m_lightning = 40;
+            item.m_itemData.m_shared.m_damages.m_spirit = 0;
+
+           var LoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Paths.PluginPath, "LightningArrows", "lightningarrows"));
+
+           if (LoadedAssetBundle == null)
+           { 
+                Debug.Log("Failed to load AssetBundle!");
+                return;
+           }
+
+           Texture2D icon = LoadedAssetBundle.LoadAsset<Texture2D>("lightningarrow-sprite.png");
+           if (icon != null)
+           {
+               Sprite sprite = Sprite.Create(icon, new Rect(0f, 0f, icon.width, icon.height), Vector2.zero);
+               //m_icons[0] is the actual sprite itself.
+               item.m_itemData.m_shared.m_icons[0] = sprite;
+           }
+
+           MeshRenderer renderer = Prefab.GetComponentInChildren<MeshRenderer>();
+
+           LoadedAssetBundle?.Unload(false);
+        }
+    }
+}
---
+++ LightningArrows/LightningArrows.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using BepInEx;
+using JotunnLib.Managers;
+using JotunnLib.Entities;
+using UnityEngine;
+
+namespace LightningArrows
+{
+    [BepInPlugin("com.bepinex.plugins.lightningarrows", "LightningArrows", "0.1.1")]
+    [BepInDependency("com.bepinex.plugins.jotunnlib")]
+    public class LightningArrows : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            ObjectManager.Instance.ObjectRegister += registerObjects;
+            PrefabManager.Instance.PrefabRegister += registerPrefabs;
+        }
+
+        private void registerPrefabs(object sender, EventArgs e)
+        {
+            PrefabManager.Instance.RegisterPrefab(new LightningArrowPrefab());
+        }
+
+        private void registerObjects(object sender, EventArgs e)
+        {
+            ObjectManager.Instance.RegisterItem("ArrowLightning");
+
+            ObjectManager.Instance.RegisterRecipe(new RecipeConfig()
+            {
+                Name = "Recipe_LightningArrows",
+                Item = "ArrowLightning",
+                Amount = 20,
+                CraftingStation = "piece_workbench",
+
+                Requirements = new PieceRequirementConfig[]
+                {
+                    new PieceRequirementConfig()
+                    {
+                        Item = "Wood",
+                        Amount = 8
+                    },
+                    new PieceRequirementConfig()
+                    {
+                        Item = "Feathers",
+                        Amount = 2
+                    },
+                    new PieceRequirementConfig()
+                    {
+                        Item = "HardAntler",
+                        Amount = 1
+                    },
+                }
+            });
+        }
+    }
+}
